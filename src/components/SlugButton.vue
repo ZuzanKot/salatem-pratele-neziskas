27aca06902328a55f2161dfcc829a1aef55caf3e
Jarka Pachlová <jarka.p@gmail.com>
2023-01-12 00:44:50 +0100

    add slug

---
+++ src/components/SlugButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+const image = new URL('/src/assets/slug.png', import.meta.url)
+</script>
+
+<template>
+    <button @click="$emit('clicked')" class="cursor-pointer">
+        <img :src="image.href" alt="Slimák" />
+    </button>
+</template>
