65a36a8ac331d40a1790babee93761177f843a4f
Jarka Pachlová <jarka.p@gmail.com>
2023-01-11 23:54:28 +0100

    rename

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { ref, watch } from 'vue'
 import Intro from '@/components/Scene/Intro.vue'
 import Cover from '@/components/Scene/Cover.vue'
 import SceneContainer from '@/components/SceneContainer.vue'
-import DialogOne from '@/components/Scene/DialogOne.vue'
+import Dialog from '@/components/Scene/Dialog.vue'
 import Garden from '@/components/Scene/Garden.vue'
 import MusicPlayer from '@/components/MusicPlayer.vue'
 
@@ -44,7 +44,7 @@ function nextScene() {
                 <Intro v-if="currentScene === 1" @next-scene="nextScene" />
             </Transition>
             <Transition>
-                <DialogOne v-if="currentScene === 2" @next-scene="nextScene" />
+                <Dialog v-if="currentScene === 2" @next-scene="nextScene" />
             </Transition>
             <Transition>
                 <Garden v-if="currentScene === 3" @next-scene="nextScene" />
