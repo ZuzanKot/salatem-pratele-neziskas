070e8dade0e88f39b26c6e35d1221a4a78755e3e
Jarka Pachlová <jarka.p@gmail.com>
2023-01-16 01:02:44 +0100

    fix border

--- src/components/DialogBox.vue
+++ src/components/DialogBox.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="w-full border-dark border-b-8 border-l-8 border-r-8 bg-light">
+    <div
+        class="w-full border-dark border-b-4 border-l-4 border-r-4 sm:border-b-8 sm:border-l-8 sm:border-r-8 xl:border-b-[16px] xl:border-l-[16px] xl:border-r-[16px] bg-light"
+    >
         <slot />
     </div>
 </template>
